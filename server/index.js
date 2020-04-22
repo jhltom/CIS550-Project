899af9ce9a01a2070bef29d559172738fe90a0ba
Soon Jae Kwon <kwonsjay@gmail.com>
2020-04-22 16:27:02 -0400

    Add cleanup methods for process termination

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const routes = require("./routes.js");
 
 const app = express();
 const PORT = 8081;
@@ -6,3 +7,15 @@ const PORT = 8081;
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
+
+
+// Test endpoint
+app.get('/test', routes.test);
+
+// Define other endpoints here
+
+
+
+// Cleanup methods on process termination
+process.on('SIGINT', routes.cleanup);
+process.on('SIGTERM', routes.cleanup);
