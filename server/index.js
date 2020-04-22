62b6fc0f9b4105e0a41aba2c013330cb33b4ef2d
Zhongyang Zhuang <ZhongyangZhuang@MacBook-Pro-3.local>
2020-04-22 13:57:47 -0400

    Create branch ingredients-cuisine-zzy.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const routes = requre("./routes.js");
 
 const app = express();
 const PORT = 8081;
@@ -6,3 +7,12 @@ const PORT = 8081;
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
+
+/* ---- (ingredients) ---- */
+app.get('/ingredients', routes.getAllIngredients);
+
+/* ---- (cuisine) ---- */
+app.get('/cuisines', routes.getAllCuisines);
+
+/* ---- (matched cuisine) ---- */
+app.get('/cuisines/:cuisine', routes.getMatchedCuisine);
---
+++ server/routes.js
@@ -0,0 +1,47 @@
+/* ---- (ingredients) ---- */
+function getAllIngredients(req, res){
+  var query = `
+    SELECT DISTINCT ingredient from ingredients;
+  `;
+  connection.query(query, function(err, rows, fields){
+      if(err) console.log(err);
+      else{
+          res.json(rows);
+      }
+  });
+};
+
+/* ---- (cuisine) ---- */
+function getAllCuisines(req, res){
+  var query =`
+    SELECT distinct dishes.cuisine FROM dishes;
+  `;
+  connection.query(query, function(err, rows, fields){
+    if(err) console.log(err);
+    else{
+        res.json(rows);
+    }
+  });
+};
+
+/* ---- (matched cuisine) ---- */
+function getMatchedCuisine(req, res){
+    var inputIngredients = req.params.ingredients;
+    var query = `
+        SELECT DISTINCT d.cuisine, COUNT(d.dishid) as freq
+        FROM Dishes d
+        WHERE EXISTS(
+            SELECT * 
+            FROM MadeOf m2 JOIN Ingredients i ON m2.ingredientId = i.id
+            WHERE i.ingredient like '%${inputIngredients}%' AND  d.dishId = m2.dishId
+        )
+        GROUP BY d.cuisine
+        ORDER BY freq DESC;
+    `;
+    connection.query(query, function(err, rows, fields){
+        if(err) console.log(err);
+        else{
+            res.json(rows);
+        }
+     });
+}
