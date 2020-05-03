7f61505fe640fa81ae22db089dcae944e85a1a5a
Tom JH Lee <jhltom@seas.upenn.edu>
2020-05-03 11:46:12 -0400

    Allow search by state and cities - front end done

--- client/src/components/SearchByCuisines.js
+++ client/src/components/SearchByCuisines.js
@@ -5,6 +5,7 @@ import Select from 'react-select';
 import { Link } from "react-router-dom";
 import '../style/SearchPage.css';
 import { TiHeartFullOutline, TiLocation } from "react-icons/ti";
+import { IconContext } from "react-icons";
 
 export default class SearchByCuisines extends React.Component {
   constructor(props) {
@@ -19,74 +20,159 @@ export default class SearchByCuisines extends React.Component {
         { value: 'Mexican', label: 'Mexican' },
         { value: 'Spanish', label: 'Spanish' },
       ],
-      locationOptions: [
+      citiesOptions: [
         { value: 'PHL', label: 'Greater Phialdelphia Area' },
         { value: 'LA', label: 'Greater Los Angeles Area' },
         { value: 'SF', label: 'San Francisco Bay Area' },
         { value: 'NY', label: 'Greater New York City Area' },
       ],
+      stateOptions: [
+        { value: 'CA', label: 'CA' },
+        { value: 'CT', label: 'CT' },
+        { value: 'DE', label: 'DE' },
+        { value: 'HI', label: 'HI' },
+        { value: 'NJ', label: 'NJ' },
+        { value: 'NY', label: 'NY' },
+        { value: 'PA', label: 'PA' },
+      ],
+
       selectedCuisines: [],
       _selectedCuisines: [],
-      selectedLocation: null,
-      _selectedLocation: "",
+      selectedState: null,
+      _selectedState: "",
+      selectedCities: [],
+      _selectedCities: []
 
     }
   }
 
+  /**
+   * When component mounts: 
+   */
+  componentDidMount = async () => {
+    // await this.getCuisineTypes();
+  }
+  getCuisineTypes = async () => {
+    await fetch("http://localhost:8081/cuisineTypes/",
+      {
+        method: 'GET'
+      }).then(res => {
+        return res.json();
+      }, err => {
+        console.log(err);
+      }).then(result => {
+        let cuisineOptions = result.rows.map((row, i) => {
+          const cuisineType = row[0].trim();
+          return ({
+            value: cuisineType,
+            label: cuisineType
+          })
+        });
+        cuisineOptions.sort((x, y) => {
+          const X = x.value.toUpperCase();
+          const Y = y.value.toUpperCase();
+          return X < Y ? -1 : X > Y ? 1 : 0;
+        });
+        this.setState({ cuisineOptions });
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  /**
+   * State handlers
+   */
+
   handleCuisinesChange = (selectedCuisines) => {
     let _selectedCuisines = [];
-    selectedCuisines.map((element) =>{
-      _selectedCuisines.push(element.value);
-    })
-    this.setState({selectedCuisines, _selectedCuisines});
+    if (selectedCuisines) {
+      selectedCuisines.map((element) => {
+        _selectedCuisines.push(element.value);
+      })
+    }
+    this.setState({ selectedCuisines, _selectedCuisines });
   }
-  handleLocationChange = (selectedLocation) => {
-    this.setState({selectedLocation, _selectedLocation: selectedLocation.value});
+  handleCitiesChange = (selectedCities) => {
+    let _selectedCities = [];
+    if (selectedCities) {
+      selectedCities.map((element) => {
+        _selectedCities.push(element.value);
+      })
+    }
+    this.setState({ selectedCities, _selectedCities });
   }
-  handleSearchToggle = () => {
-    this.setState({ toggleSearch: !this.state.toggleSearch });
+  handleStateChange = (selectedState) => {
+    this.setState({ selectedState, _selectedState: selectedState.value });
   }
+
   validateSearch = () => {
     return (
-      this.state._selectedLocation.length > 0 &&
-      this.state.selectedCuisines.length > 0
+      this.state._selectedCities.length > 0 &&
+      this.state._selectedState.length > 0 &&
+      this.state._selectedCuisines.length > 0
     )
   }
 
+  /**
+   * Render function
+   */
+
   render() {
     return (
-      <div className="rows">
-        <InputGroup.Text id="inputGroupPrepend"> <TiHeartFullOutline /> </InputGroup.Text>
-        <Select
-          isMulti
-          styles={selectStyles}
-          value={this.state.selectedCuisines}
-          isSearchable
-          placeholder="Select cuisine(s) ... "
-          size={50}
-          options={this.state.cuisineOptions}
-          onChange={this.handleCuisinesChange}
-        />
-        <div > &nbsp; &nbsp;</div>
-        <InputGroup.Text id="inputGroupPrepend"> <TiLocation /> </InputGroup.Text>
-        <Select
-          styles={selectStyles}
-          value={this.state.selectedLocation}
-          placeholder="Select location ... "
-          size={50}
-          options={this.state.locationOptions}
-          onChange={this.handleLocationChange}
-        />
-        <div > &nbsp; &nbsp;</div>
-        <Link
-          to={{
-            pathname: `/Feature3`,
-            state: {// place data you want to send here!
-              selectedLocation: this.state._selectedLocation,
-              selectedCuisines: this.state._selectedCuisines
-            } 
-          }}><Button type="submit" disabled={!this.validateSearch()} > Submit </Button> 
-        </Link>
+      <div>
+
+        <div className="rows">
+          <IconContext.Provider value={{ style: { color: 'white', marginRight:'5' } }}>
+            <TiHeartFullOutline   size={35} />
+          </IconContext.Provider>
+          <Select
+            isMulti
+            styles={selectStyles}
+            value={this.state.selectedCuisines}
+            isSearchable
+            placeholder="Select you favorite cuisine(s) "
+            options={this.state.cuisineOptions}
+            onChange={this.handleCuisinesChange}
+          />
+          <div > &nbsp; &nbsp;</div>
+          <Link
+            to={{
+              pathname: `/Feature3`,
+              state: {// place data you want to send here!
+                selectedLocation: this.state._selectedLocation,
+                selectedCuisines: this.state._selectedCuisines
+              }
+            }}><Button type="submit" disabled={!this.validateSearch()} > Search </Button>
+          </Link>
+        </div>
+
+        <div className="rows3">
+          <IconContext.Provider value={{ style: { color: 'white', marginRight:'5' } }}>
+            <TiLocation size={35} />
+          </IconContext.Provider>
+          <Select
+            styles={selectStyles}
+            value={this.state.selectedState}
+            placeholder="Select State"
+            options={this.state.stateOptions}
+            onChange={this.handleStateChange}
+          />
+          <div > &nbsp; &nbsp;</div>
+          <Select
+            styles={selectStyles}
+            value={this.state.selectedCities}
+            isMulti
+            placeholder="Select Cities"
+            options={this.state.citiesOptions}
+            onChange={this.handleCitiesChange}
+          />
+          <div > &nbsp; &nbsp;</div>
+        </div>
+
+        <div className="rows">
+          
+        </div>
+
       </div>
     );
   }
@@ -96,6 +182,6 @@ export default class SearchByCuisines extends React.Component {
 const selectStyles = {
   container: base => ({
     ...base,
-    flex: 1
-  })
+    flex: 1,
+  }),
 };
