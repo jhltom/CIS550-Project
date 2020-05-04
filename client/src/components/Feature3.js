eb2d1c4bb96be5382baebb1a4592b189771e78ac
Tom JH Lee <jhltom@seas.upenn.edu>
2020-05-04 18:59:06 -0400

    Delete line to avoid merge conflict

--- client/src/components/Feature3.js
+++ client/src/components/Feature3.js
@@ -322,7 +322,6 @@ export default class Feature3 extends React.Component {
       }
     );
     const body = await response.json();
-    console.log(body.data)
     this.setState({ loading: false, restaurants: body.data });
     this.updateMap(body.data);
   }
